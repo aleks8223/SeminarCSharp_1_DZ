335b4718b6710cfc85029ec8223d0c9eb930a684
aleks8223 <aleks8223@hotmail.com>
2023-06-13 01:47:18 +0600

    Домашняя работа 2 (Какое число большее, а какое меньшее).

--- Zadacha_2/Program.cs
+++ Zadacha_2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход 
+// принимиает два числа и выдает, какое 
+// число большее, а какое меньшее.
+
+// a = 5 b = 7 -> max = 7
+// a = 2; b = 10 -> max = 10
+// a = -9 b = -3 -> max -3
+
+Console.Write("Введите первое число"  );
+int numberA = int.Parse( Console.ReadLine()!);
+
+Console.Write("Введите второе число"  );
+int numberB = int.Parse( Console.ReadLine()!);
+
+if(numberA < numberB)
+{
+    Console.WriteLine($"Максимальное число: {numberB}");
+    Console.WriteLine($"Минимальное число: {numberA}");
+}
+
