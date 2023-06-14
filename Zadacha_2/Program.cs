94c4b953371d623f92a1ef26692852bb9668e8e2
aleks8223 <aleks8223@hotmail.com>
2023-06-15 00:50:13 +0600

    Домашнее задание от Семинара знакомство C#

--- Zadacha_2/Program.cs
+++ Zadacha_2/Program.cs
@@ -3,7 +3,7 @@
 // число большее, а какое меньшее.
 
 // a = 5 b = 7 -> max = 7
-// a = 2; b = 10 -> max = 10
+// a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max -3
 
 Console.Write("Введите первое число"  );
@@ -17,4 +17,8 @@ if(numberA < numberB)
     Console.WriteLine($"Максимальное число: {numberB}");
     Console.WriteLine($"Минимальное число: {numberA}");
 }
-
+else
+{
+    Console.WriteLine($"Максимальное число: {numberA}");
+    Console.WriteLine($"Минимальное число: {numberB}");
+}
